33a224216a7254035dc18b5993d084ceb73f6796
Marco Manrique <marcomanrique@Marcos-MacBook-Pro.local>
2020-04-23 16:00:26 -0500

    New files

---
+++ Domain/Services/IClientService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TropsmartAPI.Domain.Models;
+
+namespace TropsmartAPI.Domain.Services
+{
+    public interface IClientService
+    {
+        public List<Client> GetClients();
+        public Client AddClient(Client client);
+        public Client UpdateClient(int id, Client client);
+        public int DeleteClient(int id);
+    }
+}
---
+++ Services/ClientService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TropsmartAPI.Domain.Models;
+using TropsmartAPI.Domain.Services;
+
+namespace TropsmartAPI.Services
+{
+    public class ClientService : IClientService
+    {
+
+        private List<Client> clients;
+
+        public ClientService()
+        {
+            clients = new List<Client>();
+        }
+
+        public List<Client> GetClients()
+        {
+            return clients;
+        }
+
+        public Client AddClient(Client client)
+        {
+            clients.Add(client);
+            return client;
+        }
+
+        public Client UpdateClient(int id, Client client)
+        {
+            for (var index = clients.Count - 1; index >= 0; index--)
+            {
+                if (clients[index].id == id)
+                {
+                    clients[index] = client;
+                }
+            }
+            return client;
+        }
+
+        public int DeleteClient(int id)
+        {
+            for (var index = clients.Count - 1; index >= 0; index--)
+            {
+                if (clients[index].id == id)
+                {
+                    clients.RemoveAt(index);
+                }
+            }
+
+            return id;
+        }
+
+
+    }
+}
